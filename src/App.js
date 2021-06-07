6004c5d012d009eb2e943f27826d4b3af540fb93
Tdoval <thiagodovalaraujo@gmail.com>
2021-06-07 13:01:04 -0300

    Alert message when searching

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
   async componentDidMount() {
     this.setState({loading: true});
@@ -29,6 +31,13 @@ class App extends Component {
   // Clear users from state
   clearUsers = () => this.setState({users: [], loading: false})
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({alert:{ msg, type}});
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
   render() {
 
     const { users, loading } = this.state;
@@ -37,10 +46,12 @@ class App extends Component {
       <div className="App">
         <Navbar/>
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
             />
           <Users loading={loading} users={users} />
         </div>
