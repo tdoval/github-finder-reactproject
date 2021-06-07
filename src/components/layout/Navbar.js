036cc13bc88ae5f4bd2ceddb973f9f3cdc590519
Tdoval <thiagodovalaraujo@gmail.com>
2021-06-07 14:07:08 -0300

    React Router Setup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ icon,title }) => {
@@ -8,8 +9,15 @@ const Navbar = ({ icon,title }) => {
         <div className="navbar bg-primary">
             <h1>
                 <i className={icon} /> {title}
-                
             </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </div>
     )
 }
