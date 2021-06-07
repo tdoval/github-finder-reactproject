ed35c1d8dae319159afb423430802b7d9dd7ce08
Tdoval <thiagodovalaraujo@gmail.com>
2021-06-07 17:40:41 -0300

    Repo and RepoItem Component & Data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -43,6 +44,16 @@ class App extends Component {
 
   }
 
+  // Get user repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({repos: res.data, loading: false})
+
+  }
+
   // Clear users from state
   clearUsers = () => this.setState({users: [], loading: false})
 
@@ -54,7 +65,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
 
     return (
       <Router>
@@ -76,7 +87,7 @@ class App extends Component {
             )} />
             <Route exact path='/about' component={About} />
             <Route exact path='/user/:login' render={props => (
-              <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+              <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading} />
             )} />
           </Switch>
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 
 const Users = ({users, loading}) => {
